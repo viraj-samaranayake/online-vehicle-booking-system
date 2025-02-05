eb9dd0f81f784cfe8b61fd63f02f4a7756fea532
codevrj <isuruviraj25@gmail.com>
2025-02-05 14:12:24 +0530

    initial project

---
+++ src/main/java/com/megacity/cab_booking_system/CabBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.megacity.cab_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megacity/cab_booking_system/CabBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megacity.cab_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
