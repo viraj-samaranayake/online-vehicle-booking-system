4ddd8782d52d3d7b161fd1ab9257de96101adaa2
codevrj <isuruviraj25@gmail.com>
2025-03-11 13:57:28 +0530

    updated

--- backend/src/main/java/com/megacityCabs/cabBooking/model/Booking.java
+++ backend/src/main/java/com/megacityCabs/cabBooking/model/Booking.java
@@ -1,11 +1,15 @@
 package com.megacityCabs.cabBooking.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
 @Document(collection = "bookings")
 public class Booking {
 
@@ -13,6 +17,7 @@ public class Booking {
     private String id;
     private String customerName;
     private String address;
+    private String vehicleType;
     private String phone;
     private String startLocation;
     private String destination;
@@ -20,71 +25,4 @@ public class Booking {
     private Date createdDateTime;
     private boolean bookingStatus;
 
-
-    public Booking() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getStartLocation() {
-        return startLocation;
-    }
-
-    public void setStartLocation(String startLocation) {
-        this.startLocation = startLocation;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public Date getCreatedDateTime() {
-        return createdDateTime;
-    }
-
-    public void setCreatedDateTime(Date createdDateTime) {
-        this.createdDateTime = createdDateTime;
-    }
-
-    public boolean getBookingStatus() {
-        return bookingStatus;
-    }
-
-    public void setBookingStatus(boolean bookingStatus) {
-        this.bookingStatus = bookingStatus;
-    }
 }
